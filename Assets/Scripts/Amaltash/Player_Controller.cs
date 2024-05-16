90c554b2b20aed6e853dc80f4d9ec792745a03e5
Amaltash07 <pumsinha7o@gmail.com>
2024-05-16 18:45:40 +0530

    Inventorysystem

---
+++ Assets/Scripts/Amaltash/Player_Controller.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    public InventorySystem inventory;
+
+    void Start()
+    {
+        if (inventory == null)
+        {
+            inventory = FindObjectOfType<InventorySystem>();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+      
+        if (other.CompareTag("Item"))
+        {
+            Debug.Log("colliding with the item");
+            int itemID = other.gameObject.GetComponent<itemHandler>().itemID;
+
+            if (inventory.PickupItem(itemID))
+            {
+                Destroy(other.gameObject); // Destroy item object if picked up successfully
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/itemHandler.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemHandler : MonoBehaviour
+{
+    public int itemID;
+}
