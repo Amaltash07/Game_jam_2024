452e0c4e76bb6c851dfe3fb81220ad7362cd4e86
Amaltash07 <pumsinha7o@gmail.com>
2024-05-20 22:16:08 +0530

    ala poppa

--- Assets/Scripts/Amaltash/Player_Ability.cs
+++ Assets/Scripts/Amaltash/Player_Ability.cs
@@ -49,22 +49,21 @@ public class Player_Ability : MonoBehaviour
         }
 
         useAbility(abilityID);
-        Debug.Log(abilityID);
     }
     void switchAbility(bool dir)
     {
-        if(dir)
+        if (dir)
         {
             abilityID = (abilityID + 1) % inventory.abilityCount;
         }
 
-        else if(!dir)
+        else if (!dir)
         {
             abilityID = (abilityID - 1) % inventory.abilityCount;
-            if(abilityID==-1)
+            if (abilityID == -1)
             {
                 abilityID = inventory.abilityCount + abilityID;
-            } 
+            }
         }
         updateAbility();
 
@@ -73,7 +72,7 @@ public class Player_Ability : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
-            if (abilityID == 0 && playerHealth.currentHealth < playerHealth.maxHealth) 
+            if (abilityID == 0 && playerHealth.currentHealth < playerHealth.maxHealth)
             {
                 bool used = inventory.UseItem(abilityID);
                 if (used)
@@ -87,7 +86,7 @@ public class Player_Ability : MonoBehaviour
                     Debug.Log("Ability unavailable");
                 }
             }
-            else if(abilityID==1 && !weapon.isWallBreaker)
+            else if (abilityID == 1 && !weapon.isWallBreaker)
             {
                 bool used = inventory.UseItem(abilityID);
                 if (used)
@@ -115,6 +114,10 @@ public class Player_Ability : MonoBehaviour
     void updateAbility()
     {
         inventory.changeIcon(abilityID);
+        editNumber();
+    }
+    public void editNumber()
+    {
         AbilityName.text = inventory.items[abilityID].currentPickups.ToString();
     }
 }
